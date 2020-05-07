557b67bda20d49ed48dd086155b410af8b9809d5
Corbin Robb <corbinrobb@gmail.com>
2020-05-07 12:26:51 -0600

    Refactor tests and add more tests

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,16 +12,17 @@ const ContactForm = () => {
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form data-testid="form" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
+            id="firstName"
             placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid="first-name-error">Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
@@ -29,11 +30,12 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
+            id="lastName"
             placeholder="luo"
             ref={register({ required: true })}
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p data-testid="last-name-error">Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
 
@@ -41,21 +43,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" id="email" ref={register({ required: true })} />
           {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
+            <p data-testid="email-error">Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" id="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid="json" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input aria-label="submit" type="submit" />
       </form>
     </div>
   );
---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { render, fireEvent, queryByText } from "@testing-library/react";
+import ContactForm from './ContactForm';
+
+test("checks if inputs are working and submit fires with no error messages", async () => {
+  const { getByLabelText, findByTestId, queryByTestId } = render(<ContactForm />);
+  const firstName = getByLabelText(/first name/i);
+  const lastName = getByLabelText(/last name/i);
+  const email = getByLabelText(/email/i);
+  const message = getByLabelText(/message/i);
+  const button = getByLabelText(/submit/i);
+
+  fireEvent.change(firstName, { target: { value: "David" } });
+  fireEvent.change(lastName, { target: { value: "Smith" } });
+  fireEvent.change(email, { target: { value: "davidsmith@email.com" } });
+  fireEvent.change(message, { target: { value: "lorem ipsum" } });
+
+  expect(firstName.value).toBe("David");
+  expect(lastName.value).toBe("Smith");
+  expect(email.value).toBe("davidsmith@email.com");
+  expect(message.value).toBe("lorem ipsum");
+
+  fireEvent.submit(button);
+
+  expect(await findByTestId(/json/i)).toHaveTextContent(`{ "firstName": "David", "lastName": "Smith", "email": "davidsmith@email.com", "message": "lorem ipsum" }`)
+  expect(queryByTestId(/first-name-error/i)).toBeNull();
+  expect(queryByTestId(/last-name-error/i)).toBeNull();
+  expect(queryByTestId(/email-error/i)).toBeNull();
+});
+
+test("checks if errors messages are coming up", async () => {
+  const { getByLabelText, findByTestId, queryByTestId } = render(<ContactForm />);
+  const firstName = getByLabelText(/first name/i);
+  const lastName = getByLabelText(/last name/i);
+  const email = getByLabelText(/email/i);
+  const button = getByLabelText(/submit/i);
+
+  fireEvent.change(firstName, { target: { value: "" } });
+  fireEvent.change(lastName, { target: { value: "" } });
+  fireEvent.change(email, { target: { value: "" } });
+
+  expect(firstName.value).toBe("");
+  expect(lastName.value).toBe("");
+  expect(email.value).toBe("");
+  expect(message.value).toBe("");
+
+  fireEvent.submit(button);
+
+  expect(queryByTestId(/json/i)).toBeNull();
+
+  expect(await findByTestId(/first-name-error/i)).toHaveTextContent("Looks like there was an error: required");
+  expect(await findByTestId(/last-name-error/i)).toHaveTextContent("Looks like there was an error: required");
+  expect(await findByTestId(/email-error/i)).toHaveTextContent("Looks like there was an error: required");
+
+  fireEvent.change(firstName, { target: { value: "D" } });
+  fireEvent.submit(button);
+
+  expect(await findByTestId(/first-name-error/i)).toHaveTextContent("Looks like there was an error: minLength");
+});
